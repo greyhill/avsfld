8eecf7663bbd23d7515605a33bf27509d38144c8
Madison McGaffin <greyhill@gmail.com>
2014-03-05 11:13:50 -0500

    restructured stuff for distutils

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+        name = 'avsfld',
+        version = '0.0.1',
+        author = 'Madison McGaffin',
+        author_email = 'greyhill@gmail.com',
+        packages = [ 'avsfld' ],
+        scripts = [ 'bin/fld_to_le.py' ],
+        url = 'http://github.com/greyhill/avsfld',
+        description = 'Read/write AVS .fld files',
+        install_requires = [
+            'numpy',
+            ],
+        )
+
